c2a2cafa12326dd9272e45c75ac55e675e9c1129
Guccio163 <wiktor.gut@op.pl>
2022-12-20 17:23:27 +0100

    worldmap fix

--- EvolutionGenerator/src/main/java/Enums/WorldMapDirection.java
+++ EvolutionGenerator/src/main/java/Enums/WorldMapDirection.java
@@ -1,68 +1,48 @@
 package Enums;
 
+import Tools.Vector2d;
+
 import java.util.concurrent.ThreadLocalRandom;
 
-public enum MapDirection {
+public enum WorldMapDirection {
     NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST;
 
-    public static MapDirection getRandom() {
-        return MapDirection.values()[ThreadLocalRandom.current().nextInt(7)];
+    public static WorldMapDirection getRandom() {
+        return WorldMapDirection.values()[ThreadLocalRandom.current().nextInt(7)];
     }
 
     public String toString() {
-        switch (this) {
-            case NORTH:
-                return "N";
-            case NORTHWEST:
-                return "NW";
-            case WEST:
-                return "W";
-            case SOUTHWEST:
-                return "SW";
-            case SOUTH:
-                return "S";
-            case SOUTHEAST:
-                return "SE";
-            case EAST:
-                return "E";
-            case NORTHEAST:
-                return "NE";
-            default:
-                return null;
-        }
-        return null;
+        return switch (this) {
+            case NORTH -> "N";
+            case NORTHWEST -> "NW";
+            case WEST -> "W";
+            case SOUTHWEST -> "SW";
+            case SOUTH -> "S";
+            case SOUTHEAST -> "SE";
+            case EAST -> "E";
+            case NORTHEAST -> "NE";
+        };
     }
 
-    public MapDirection next() {
-        return MapDirection.values()[(this.ordinal() + 1) % MapDirection.values().length];
+    public WorldMapDirection next() {
+        return WorldMapDirection.values()[(this.ordinal() + 1) % WorldMapDirection.values().length];
     }
 
-    public MapDirection previous() {
-        return MapDirection.values()[(this.ordinal() + MapDirection.values().length - 1) % MapDirection.values().length];
+    public WorldMapDirection previous() {
+        return WorldMapDirection.values()[(this.ordinal() + WorldMapDirection.values().length - 1) % WorldMapDirection.values().length];
     }
 
     public Vector2d toUnitVector() {
-        switch (this) {
-            case NORTH:
-                return new Vector2d(0, 1);
-            case NORTHEAST:
-                return new Vector2d(1, 1);
-            case EAST:
-                return new Vector2d(1, 0);
-            case SOUTHEAST:
-                return new Vector2d(1, -1);
-            case SOUTH:
-                return new Vector2d(0, -1);
-            case SOUTHWEST:
-                return new Vector2d(-1, -1);
-            case WEST:
-                return new Vector2d(-1, 0);
-            case NORTHWEST:
-                return new Vector2d(-1, 1);
-            default:
-                return new Vector2d(0, 0);
-        }
-        return new Vector2d(0, 0);
+        return switch (this) {
+            case NORTH -> new Vector2d(0, 1);
+            case NORTHEAST -> new Vector2d(1, 1);
+            case EAST -> new Vector2d(1, 0);
+            case SOUTHEAST -> new Vector2d(1, -1);
+            case SOUTH -> new Vector2d(0, -1);
+            case SOUTHWEST -> new Vector2d(-1, -1);
+            case WEST -> new Vector2d(-1, 0);
+            case NORTHWEST -> new Vector2d(-1, 1);
+        };
     }
 
 }
