71c6c2a66d77b83ceedbf745d0899e77a20fffcf
psp515 <kolberu@gmail.com>
2022-12-29 17:45:38 +0100

    Update MapDirection.java

--- EvolutionGenerator/src/main/java/Enums/MapDirection.java
+++ EvolutionGenerator/src/main/java/Enums/MapDirection.java
@@ -18,7 +18,7 @@ public enum MapDirection {
         return MapDirection.values()[ThreadLocalRandom.current().nextInt(7)];
     }
 
-    public MapDirection whatever(){return MapDirection.EAST}
+    public MapDirection whatever(){return MapDirection.EAST;}
 
     public MapDirection getOpposed(){
         return switch (this) {
