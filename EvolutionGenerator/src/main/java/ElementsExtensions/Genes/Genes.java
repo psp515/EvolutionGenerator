4ef454488cf3823f85b3bbace50e46ea9a9c445e
Guccio163 <wiktor.gut@op.pl>
2022-12-30 02:29:23 +0100

    basic stuff

--- EvolutionGenerator/src/main/java/ElementsExtensions/Genes/Genes.java
+++ EvolutionGenerator/src/main/java/ElementsExtensions/Genes/Genes.java
@@ -3,6 +3,8 @@ package ElementsExtensions.Genes;
 import Elements.Animal;
 import Interfaces.Animals.IGenes;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public abstract class Genes implements IGenes {
     private int[] genes;
     private int activatedGene;
@@ -13,23 +15,26 @@ public abstract class Genes implements IGenes {
         mutate();
     }
 
-    @Override
-    public int[] getGenes() {
-        return genes;
-    }
-    @Override
-    public void mutate() {
-        this.genes = null;
+    public Genes(Animal parent1, Animal parent2, int genLength){
+        int part1 = (genLength*parent1.getEnergy())/(parent1.getEnergy()+parent2.getEnergy());
+        int side1 = ThreadLocalRandom.current().nextInt(2);
+        int[] childgenes = new int[genLength];
+
+        for(int i=0; i<part1; i++) {
+            childgenes[i] = side1*parent1._genotype.getGenes()[i] + (1-side1)*parent2._genotype.getGenes()[i];
+        }
+        for(int i=part1; i<genLength; i++){
+            childgenes[i] = (1-side1)*parent1._genotype.getGenes()[i] + side1*parent2._genotype.getGenes()[i];
+        }
+
+        this.genes = childgenes;
+        mutate();
     }
 
     @Override
-    public int[] generateGenes(Animal parent1, Animal parent2, int genLength) {
-        return null;
-    }
-
+    public int[] getGenes() { return genes; }
     @Override
-    public int getActiveGene() {
-        int gen = this.activatedGene;
-        return gen;
-    }
+    public int getActiveGene() { return this.activatedGene; }
+    @Override
+    public void mutate() { this.genes = null; }
 }
